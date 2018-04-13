4bf023ffa64946ab50f4e1c02866f0ed54951ef1
xuepeng <helloxuepeng@gmail.com>
2018-04-13 16:41:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xuepeng/fragmenttest/LeftFragment.java
@@ -0,0 +1,10 @@
+package com.example.xuepeng.fragmenttest;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by xuepeng on 18/4/13.
+ */
+
+public class LeftFragment extends Fragment {
+}
